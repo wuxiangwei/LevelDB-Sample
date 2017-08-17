034f2acf4c89e6e1a5be7e277c0a3e883ec2aa31
root <root@localhost>
2017-08-17 13:57:07 +0800

    refact

--- sample.cpp
+++ sample.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-
 #include "leveldb/db.h"
 
-using namespace std;
 
 int main(int argc, char** argv)
 {
@@ -14,44 +12,36 @@ int main(int argc, char** argv)
     options.create_if_missing = true;
 
     leveldb::Status status = leveldb::DB::Open(options, "./testdb", &db);
-
-    if (false == status.ok())
-    {
-        cerr << "Unable to open/create test database './testdb'" << endl;
-        cerr << status.ToString() << endl;
+    if (false == status.ok()) {
+        std::cerr << "Unable to open/create test database './testdb'" << std::endl;
+        std::cerr << status.ToString() << std::endl;
         return -1;
     }
     
     // Add 256 values to the database
     leveldb::WriteOptions writeOptions;
-    for (unsigned int i = 0; i < 256; ++i)
-    {
-        ostringstream keyStream;
+    for (unsigned int i = 0; i < 256; ++i) {
+        std::ostringstream keyStream;
+        std::ostringstream valueStream;
+
         keyStream << "Key" << i;
-        
-        ostringstream valueStream;
         valueStream << "Test data value: " << i;
-        
         db->Put(writeOptions, keyStream.str(), valueStream.str());
     }
     
     // Iterate over each item in the database and print them
     leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
-    
-    for (it->SeekToFirst(); it->Valid(); it->Next())
-    {
-        cout << it->key().ToString() << " : " << it->value().ToString() << endl;
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        std::cout << it->key().ToString() << " : " << it->value().ToString() << std::endl;
     }
     
-    if (false == it->status().ok())
-    {
-        cerr << "An error was found during the scan" << endl;
-        cerr << it->status().ToString() << endl; 
+    if (false == it->status().ok()) {
+        std::cerr << "An error was found during the scan" << std::endl;
+        std::cerr << it->status().ToString() << std::endl;
     }
     
     delete it;
-    
-    // Close the database
-    delete db;
-}
+    delete db;  // Close the database
 
+    return 0;
+}
