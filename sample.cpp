73a28a45515be0b74a08df713b0e544a83eb5e80
wuxiangwei <shanno@yeah.net>
2017-08-17 16:38:49 +0800

    测试leveldb compactrange问题

--- sample.cpp
+++ sample.cpp
@@ -7,13 +7,30 @@
 #include "leveldb/write_batch.h"
 
 
-
-void compact_range(leveldb::DB *db, const std::string& start, const std::string& end) {
-    // TODO: check db
+// compact
+void compact_range(leveldb::DB *db, 
+    const std::string& start, const std::string& end, bool write_null=false) {
     leveldb::Slice cstart(start);
     leveldb::Slice cend(end);
-    db->Write(leveldb::WriteOptions(), NULL);
+    if (write_null) {
+        db->Write(leveldb::WriteOptions(), NULL);
+        std::cout << "write NULL" << std::endl;
+    }
     db->CompactRange(&cstart, &cend);
+    std::cout << "compact_range(" << start << ", " << end << ", " << write_null << ")" << std::endl;
+}
+
+
+char* gen_value(const std::string& key) {
+    std::string fn = "./testdata/" + key;
+    std::ifstream in(fn.c_str(), std::ios::in|std::ios::binary|std::ios::ate);
+    size_t sz = in.tellg();
+    in.seekg(0, std::ios::beg);
+    std::cout << "Size: "<< sz << std::endl;
+    char* buffer = new char[sz];
+    in.read(buffer, sz);
+    in.close();
+    return buffer;
 }
 
 
@@ -24,72 +41,48 @@ int main(int argc, char** argv)
     options.create_if_missing = true;
 
     leveldb::Status status = leveldb::DB::Open(options, "./testdb", &db);
-    if (false == status.ok()) {
+    if (!status.ok()) {
         std::cerr << "Unable to open/create test database './testdb'" << std::endl;
         std::cerr << status.ToString() << std::endl;
         return -1;
     }
 
+    const unsigned KEY_NUM = 4;
+    char *buffers[KEY_NUM];
     leveldb::WriteBatch batch;
-    char *buffers[4] = {NULL, NULL, NULL, NULL};
-    for (int i=1; i<5; ++i) {
-        std::ostringstream keyStream;
-        // std::ostringstream valueStream;
-        keyStream << "Key" << i;
-        // valueStream << "Test data value: " << i;
-        // db->Put(writeOptions, keyStream.str(), valueStream.str());
-        char *buffer = NULL;
-        {
-            std::stringstream fn;
-            fn << i << ".txt";
-            std::cout << fn.str() << std::endl;
-            std::string fname = fn.str();
-            std::cout << fname.c_str() << std::endl;
-            std::ifstream in(fname.c_str(), std::ios::in|std::ios::binary|std::ios::ate);
-            size_t sz = in.tellg();
-            in.seekg(0, std::ios::beg);
-            std::cout << "Size: "<< sz << std::endl;
-            buffer = new char[sz];
-            in.read(buffer, sz);
-            in.close();
-        }
 
-        if (buffer) {
-            buffers[i-1] = buffer;
-            batch.Put(keyStream.str(), buffers[i-1]);
-        }
+    // write first
+    for (int i=0; i<KEY_NUM; i++) {
+        std::ostringstream key;
+        key << i+1 << ".txt";
+        buffers[i] = gen_value(key.str());
+        batch.Put(key.str(), buffers[i]);
     }
     db->Write(leveldb::WriteOptions(), &batch);
 
-    // delete keys
-    leveldb::WriteBatch bat;
-    for (int i=1; i<5; i++) {
+    // delete then
+    batch.Clear();
+    for (int i=0; i<KEY_NUM; i++) {
         std::ostringstream key;
-        key << "Key" << i;
-        bat.Delete(key.str());
+        key << i+1 << ".txt";
+        batch.Delete(key.str());
     }
-    db->Write(leveldb::WriteOptions(), &bat);
+    db->Write(leveldb::WriteOptions(), &batch);
 
-    std::string start("Key1");
-    std::string end("Key4");
+    std::string start("1.txt");
+    std::string end("4.txt");
+    // case1: Big
     // db->CompactRange(NULL, NULL);
+    // case2: OK
+    // compact_range(db, start, end, true);
+    // case3: Big
     compact_range(db, start, end);
+    // case4: compact twice, OK
+    // compact_range(db, start, end);
     // compact_range(db, start, end);
     
-    // Iterate over each item in the database and print them
-    // leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
-    // for (it->SeekToFirst(); it->Valid(); it->Next()) {
-    //     std::cout << it->key().ToString() << " : " << it->value().ToString() << std::endl;
-    // }
-    
-    // if (false == it->status().ok()) {
-    //     std::cerr << "An error was found during the scan" << std::endl;
-    //     std::cerr << it->status().ToString() << std::endl;
-    // }
-    
-    // delete it;
     delete db;  // Close the database
-    for (int i=0; i<4; i++) {
+    for (int i=0; i<KEY_NUM; i++) {
         if (NULL != buffers[i]) {
             delete buffers[i];
         }
